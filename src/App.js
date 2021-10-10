92bb312f40bb156d0f3312b874868f4ef25762ed
Matias Lang <matiaslang@icloud.com>
2021-10-10 11:19:29 +0300

    Implementing better table from materialUI

--- src/App.js
+++ src/App.js
@@ -15,19 +15,14 @@ import {
   TableCell,
   TableHead,
   TableRow,
+  Button,
 } from '@mui/material'
 import { makeStyles } from '@material-ui/core/styles'
 import SearchBar from 'material-ui-search-bar'
 import CustomListItem from './List/CustomListItem'
 import { useMutation, useQuery } from 'react-query'
-import ky from 'ky'
 import axios from 'axios'
-
-const api = ky.create({
-  prefixUrl: '/api',
-  retry: 3,
-  timeout: 60000,
-})
+import DataTable from './List/DataTable'
 
 const useStyles = makeStyles({
   table: {
@@ -44,18 +39,6 @@ async function fetchData() {
     console.error(error)
   }
   return result
-}
-
-function Names() {
-  const { data, error, isError, isLoading } = useQuery('names', fetchData)
-  if (isLoading) {
-    return <div>Loading...</div>
-  }
-  if (isError) {
-    return <div>Error! {error.message}</div>
-  }
-
-  return <div className='container'>{data}</div>
 }
 
 const originalItems = [
@@ -101,51 +84,41 @@ function App() {
     requestSearch(searched)
   }
 
+  const DataLoaded = ({ mutation }) => {
+    if (mutation.isLoading) {
+      return <div>Loading data....</div>
+    }
+    if (mutation.isError) {
+      return <div>There was an error: {mutation.error}</div>
+    }
+    if (mutation.isSuccess) {
+      return <DataTable data={mutation.data.data} />
+    }
+    return null
+  }
+
   return (
     <div className='App'>
       <Paper elevation={3}>
         <SearchBar
+          style={{ padding: 10 }}
           value={searched}
           onChange={(searchVal) => requestSearch(searchVal)}
           onCancelSearch={() => cancelSearch()}
           placeholder='filter'
         />
-        <TableContainer>
-          <Table className={classes.table} aria-label='simple table'>
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell align='right'>Amount</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {mutation.isLoading ? (
-                'Fetching the list....'
-              ) : (
-                <>
-                  {mutation.isError ? (
-                    <div> An Error occurred: {mutation.error.message}</div>
-                  ) : null}
-                  {mutation.isSuccess
-                    ? names.slice(0, 10).map((m) => <CustomListItem data={m} />)
-                    : null}
-                  <Divider variant='inset' component='li' />
-
-                  <button
-                    onClick={() => {
-                      mutation.mutate({
-                        id: new Date(),
-                        title: 'Fetch names from DB',
-                      })
-                    }}
-                  >
-                    Fetch names from DB
-                  </button>
-                </>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <DataLoaded mutation={mutation} />
+        <Button
+          variant='contained'
+          onClick={() => {
+            mutation.mutate({
+              id: new Date(),
+              title: 'Fetch names from DB',
+            })
+          }}
+        >
+          Fetch names from DB
+        </Button>
       </Paper>
     </div>
   )
